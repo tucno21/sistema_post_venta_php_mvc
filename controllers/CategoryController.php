7f7637fda1fa29fa7cfe82b144414c60ca987355
tucno21 <carlitostucno@gmail.com>
2021-08-23 10:22:03 -0500

    actualizar categoria

--- controllers/CategoryController.php
+++ controllers/CategoryController.php
@@ -52,4 +52,47 @@ class CategoryController
             'errores' => $errores,
         ]);
     }
+
+    public static function actualizar(Router $router)
+    {
+        $errores = [];
+        $id = validarORedireccionar('/categorias');
+        $valorColum = $id;
+        //busscar usuario y traer como objeto
+        $categoria = Categories::find($valorColum);
+
+        if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+            if (!$_POST['categoria']['category']) {
+                array_push($errores, "La categoria es obligatorio");
+            }
+            if (
+                preg_match('/^[a-zA-z0-9ñÑáéíóúÁÉÍÓÚ ]+$/', $_POST['categoria']['category'])
+            ) {
+                //Buscar ususario y traer
+                $colum =  "category";
+                $valorColum = $_POST['categoria']["category"];
+                $respuesta = Categories::FindColumn($colum, $valorColum);
+                $nombre = isset($respuesta->category);
+                if (!$nombre) {
+                    if (empty($errores)) {
+                        $args = $_POST['categoria'];
+                        $respuesta = Categories::update($args, $id);
+
+                        if ($respuesta == "ok") {
+                            header('Location: /categorias');
+                        }
+                    }
+                } else {
+                    array_push($errores, "La categoria ya existe!");
+                }
+            } else {
+                array_push($errores, "Caracteres no admitidos, ingrese caracteres A-Z y/o 0-9");
+            }
+        }
+
+        $router->render('categorias/actualizar', [
+            'errores' => $errores,
+            'categoria' => $categoria,
+        ]);
+    }
 }
